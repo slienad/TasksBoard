edafe3cae3624c272bc8edc22369e32a50e7d8d6
slienad <daniel.sarnel77@orange.fr>
2014-02-19 12:09:10 +0100

    Unicorn modification

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,9 @@
-root = "/home/deployer/apps/TaksBoard/current"
+root = "/home/deployer/apps/Tasksboard/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/tmp/unicorn.TasksBoard.sock"
+listen "/tmp/unicorn.Tasksboard.sock"
 worker_processes 2
 timeout 30
